f988f19e8924bf1fc9b7e6f4fcbdeb43ffc31c1a
LUCAS-PC\Lucas <lucaskchang@gmail.com>
2023-08-28 20:26:31 -0700

    clubs sign up

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -135,9 +135,9 @@
 
     <!-- lunch menu -->
     <q-dialog v-model="lunchMenu">
-      <q-img style="min-height: 90vh; min-width:40vw; overflow: hidden" src="menu/1.jpg" />
+      <q-img style="min-height: 90vh; min-width:40vw;" src="menu/1.jpg" />
       <br />
-      <q-img style="min-height: 90vh; min-width:40vw; overflow: hidden" src="menu/2.jpg" />
+      <q-img style="min-height: 90vh; min-width:40vw;" src="menu/2.jpg" />
     </q-dialog>
 
     <!-- custom schedule menu -->
