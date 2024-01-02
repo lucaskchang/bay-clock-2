725ab6ea74d8269fa978f3d4997e2f4995321cf8
LUCAS-PCLucas <lucaskchang@gmail.com>
2024-01-02 12:33:01 -0800

    immersive + lunch menu

--- assets/BackButton.9c127601.js
+++
@@ -1,1 +0,0 @@
-import{a2 as a,aY as o,a4 as e,a5 as t,ad as c,s as n}from"./index.98216a06.js";const s=a({name:"BackButton"}),r={class:"fixed-bottom-left q-ma-md"};function d(i,m,p,_,B,l){return e(),t("div",r,[c(n,{color:"primary",label:"Back to Bay Clock",icon:"arrow_back",size:"md",to:"/",rounded:"","no-caps":""})])}var k=o(s,[["render",d]]);export{k as B};
