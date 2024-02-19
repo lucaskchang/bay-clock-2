ab193b249f77763b97c1b89f2d7cd0ae2e67dcd0
LUCAS-PC\Lucas <lucaskchang@gmail.com>
2024-02-19 13:05:35 -0800

    fix

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -814,18 +814,19 @@ const immersive = computed<boolean>(function () {
 });
 
 // computes whether today is in a holiday and returns name if so
-const holiday = computed<string>(function () {
-  var now = time.value;
-  for (const [name, info] of Object.entries(holidays)) {
-    if (
-      now > new Date(now.getFullYear() + "/" + info["start"]) &&
-      now < new Date(now.getFullYear() + "/" + info["end"])
-    ) {
-      return name;
-    }
-  }
-  return "";
-});
+const holiday = ""
+// const holiday = computed<string>(function () {
+//   var now = time.value;
+//   for (const [name, info] of Object.entries(holidays)) {
+//     if (
+//       now > new Date(now.getFullYear() + "/" + info["start"]) &&
+//       now < new Date(now.getFullYear() + "/" + info["end"])
+//     ) {
+//       return name;
+//     }
+//   }
+//   return "";
+// });
 
 // computes the current day's schedule
 const scheduleDictionary = computed<ParsedScheduleType>(function () {
@@ -1348,10 +1349,10 @@ watch(showGraduationCountdown, (state: boolean) => {
 
 // load local storage once mounted
 // HOLIDAY STUFF
-const icons = []
-for (let i = 0; i < 51; i++) {
-  icons.push(holidays[holiday.value]["icons"][Math.floor(Math.random() * holidays[holiday.value]["icons"].length)])
-}
+// const icons = []
+// for (let i = 0; i < 51; i++) {
+//   icons.push(holidays[holiday.value]["icons"][Math.floor(Math.random() * holidays[holiday.value]["icons"].length)])
+// }
 
 onMounted(() => {
   var check_custom_blocks = <ScheduleType>$q.localStorage.getItem("custom_blocks");
